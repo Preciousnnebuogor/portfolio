0381894bfc0eaa03f88bd8daeca3236f6856727b
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-10-23 22:34:46 -0700

    added service.tsx

--- app/service.tsx
+++ app/service.tsx
@@ -1,15 +1,51 @@
 "use client"
 export default function Service (){
-    return <div>
-        <div>
-            <div className={`text-2xl font-bold flex items-center justify-center`}>
-                <h1>My Services</h1>
+    return (
+      <div className={`flex item-center justify-center w-full py-5 h-screen`}>
+        <div className={`w-[80%] flex flex-col justify-between gap-x-4`}>
+          <div
+            className={`flex items-center justify-center text-2xl font-bold `}
+          >
+            <h1>My Services</h1>
+          </div>
+          <div className={`flex gap-4 justify-between h-[80%] `}>
+            <div
+              className={`bg-slate-900 hover:bg-blue-900 px-3 flex flex-col items-center justify-center`}
+            >
+              <img src="/nr.png" width="260" height="200"></img>
+              <p>
+                Ensuring websites look and work well across different devices
+                (mobile, tablet, desktop) by using responsive frameworks (like
+                Bootstrap) or media queries.Making sure the website behaves
+                consistently across all major web browsers (Chrome, Firefox,
+                Safari, Edge, etc.).
+              </p>
             </div>
-            <div className={`grid grid-cols-3`}>
-             <div className={`bg-slate-900 `}>
-             
-             </div>
+            <div
+              className={`bg-slate-900 hover:bg-blue-900 px-3 flex flex-col items-center justify-center`}
+            >
+              <img src="/nt.png" width="260" height="200"></img>
+              <p>
+                Minimizing loading times by optimizing images, lazy loading,
+                minifying CSS/JS, and implementing code-splitting. Ensuring that
+                websites are accessible to all users, including people with
+                disabilities, by following accessibility guidelines (e.g.,
+                WCAG).
+              </p>
             </div>
+            <div
+              className={`bg-slate-900 hover:bg-blue-900 px-3 flex flex-col items-center justify-center`}
+            >
+              <img src="/na.png" width="260" height="200"></img>
+              <p>
+                CSS animations and transitions: Adding visual effects and
+                animations to improve user engagement. JavaScript/jQuery
+                animations: Creating interactive elements like sliders,
+                carousels, modals, and pop-up features.
+              </p>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+    );
 }
